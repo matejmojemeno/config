7d1fc6dcd6b8b16b0a6ebfcac58eb1c5351d890d
matejmojemeno <matej.kulhan38@gmail.com>
2024-03-10 00:58:02 +0100

    Fixed completing one word or one line

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,21 +1,8 @@
 return {
-    "github/copilot.vim",
+	"github/copilot.vim",
 
-    config = function ()
-        local function SuggestOneWord()
-          local suggestion = vim.fn['copilot#Accept']("")
-          local bar = vim.fn['copilot#TextQueuedForInsertion']()
-          return bar:match("%S+")
-        end
-
-        local function SuggestOneLine()
-          local suggestion = vim.fn['copilot#Accept']("")
-          local bar = vim.fn['copilot#TextQueuedForInsertion']()
-          return bar:match("[^\n]+")
-        end
-
-        local map = vim.keymap.set
-        map('i', '<C-w>', SuggestOneWord, {expr = true, remap = false})
-        map('i', '<C-l>', SuggestOneLine, {expr = true, remap = false})
-    end,
+	config = function()
+		vim.keymap.set("i", "<C-w>", "<Plug>(copilot-accept-word)")
+		vim.keymap.set("i", "<C-;>", "<Plug>(copilot-accept-line)")
+	end,
 }
