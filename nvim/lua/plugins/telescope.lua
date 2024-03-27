5e95d134468386775e67ad0141d39860c2bbb81c
matejmojemeno <matej.kulhan38@gmail.com>
2024-03-27 14:20:45 +0100

    Tweaks

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,10 @@
-return { -- Fuzzy Finder (files, lsp, etc)
+return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ -- If encountering errors, see telescope-fzf-native README for install instructions
+		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 
 			build = "make",
