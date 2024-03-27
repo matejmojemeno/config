0e4d6d9a4e44588dc770c137a233b095a8b47a51
matejmojemeno <matej.kulhan38@gmail.com>
2024-03-27 14:20:12 +0100

    Doesn't show output automatically now

--- nvim/lua/plugins/magma.lua
+++ nvim/lua/plugins/magma.lua
@@ -1,29 +1,29 @@
 return {
-    {
-        "dccsillag/magma-nvim",
-        build = ":UpdateRemotePlugins",
+	{
+		"dccsillag/magma-nvim",
+		build = ":UpdateRemotePlugins",
 
-        config = function()
-            -- vim.api.nvim_set_keymap('n', '<LocalLeader>r', ':MagmaEvaluateOperator<CR>', { silent = true, expr = true })
-            vim.api.nvim_set_keymap('n', '<LocalLeader>rr', ':MagmaEvaluateLine<CR>', { silent = true })
-            vim.api.nvim_set_keymap('x', '<LocalLeader>r', ':<C-u>MagmaEvaluateVisual<CR>', { silent = true })
-            vim.api.nvim_set_keymap('n', '<LocalLeader>rc', ':MagmaReevaluateCell<CR>', { silent = true })
-            vim.api.nvim_set_keymap('n', '<LocalLeader>rd', ':MagmaDelete<CR>', { silent = true })
-            vim.api.nvim_set_keymap('n', '<LocalLeader>ro', ':MagmaShowOutput<CR>', { silent = true })
+		config = function()
+			-- vim.api.nvim_set_keymap('n', '<LocalLeader>r', ':MagmaEvaluateOperator<CR>', { silent = true, expr = true })
+			vim.api.nvim_set_keymap("n", "<LocalLeader>rr", ":MagmaEvaluateLine<CR>", { silent = true })
+			vim.api.nvim_set_keymap("x", "<LocalLeader>r", ":<C-u>MagmaEvaluateVisual<CR>", { silent = true })
+			vim.api.nvim_set_keymap("n", "<LocalLeader>rc", ":MagmaReevaluateCell<CR>", { silent = true })
+			vim.api.nvim_set_keymap("n", "<LocalLeader>rd", ":MagmaDelete<CR>", { silent = true })
+			vim.api.nvim_set_keymap("n", "<LocalLeader>ro", ":MagmaShowOutput<CR>", { silent = true })
 
-            vim.g.magma_automatically_open_output = true
-            vim.g.magma_image_provider = "kitty"
+			vim.g.magma_automatically_open_output = false
+			vim.g.magma_image_provider = "kitty"
 
-            function MagmaInitPython()
-                vim.cmd[[
+			function MagmaInitPython()
+				vim.cmd([[
                 :MagmaInit python3
                 :MagmaEvaluateArgument a=5
-                ]]
-            end
+                ]])
+			end
 
-            vim.cmd[[
+			vim.cmd([[
                 command! MagmaInitPython lua MagmaInitPython()
-            ]]
-        end
-    }
+            ]])
+		end,
+	},
 }
